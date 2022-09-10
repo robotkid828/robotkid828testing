4e79b6ad5a7f68e43a551313d06828371514d75a
robotkid828 <113220796+robotkid828@users.noreply.github.com>
2022-09-09 22:08:50 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from my_pip_package import __version__
+
+setup(
+    name='test.py',
+    version=__version__,
+
+    url='https://github.com/robotkid828/robotkid828testing',
+    author='robotkid828',
+    author_email='',
+
+    py_modules=['test'],
+)
